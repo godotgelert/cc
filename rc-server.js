8a1f9bdfdfa878e1840377449cbd84933f25cf95
Robin Duckett <robin.duckett@gmail.com>
2021-01-25 10:57:10 +0000

    RC

--- rc-server.js
+++ rc-server.js
@@ -2,10 +2,34 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 9397 });
 
+var connected = [];
+
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
+    if (message.startsWith("iden")) {
+      connected.push({
+        ws,
+        id: message.split(" ")[1],
+      });
+
+      console.log("cc connected: ", connected[connected.length - 1].id);
+    }
+
+    if (message.startsWith("pong")) {
+      console.log("cc ponged: ", connected[connected.length - 1].id);
+    }
   });
 
-  ws.send("something");
+  ws.send("ping");
+
+  ws.send("save test.lua print('hello world!')\nprint('byeee')");
+  ws.send("runf test.lua");
+
+  ws.on("close", function closed() {
+    let todelete = connected.findIndex((c) => {
+      return c.ws === ws;
+    });
+
+    connected.splice(todelete, 1);
+  });
 });
--- rc.lua
+++ rc.lua
@@ -1,10 +1,68 @@
-local ws, err = http.websocket("ws://81.2.127.172:9397/")
+local ws, err
+
+function connect()
+  ws, err = http.websocket("ws://81.2.127.172:9397/")
+end
+
+function save_file(data)
+  local words = {}
+  for word in s:gmatch("%w+") do table.insert(words, word) end
+  local file = io.open(words[0], "w")
+  file:write(strsub(data, words[0].len() + 1))
+  file:close()
+end
+
+function run_file(data)
+  local output = shell(data)
+  
+  local file = io.open("output.tmp", "r")
+  local output_data = ""
+  local line = file:read()
+  
+  repeat
+    line = file:read()
+    output_data = output_data .. line
+  until line == nil
+  file:close()
+
+  ws.send(output_data)
+end
+
+--- rpc handler
+function handle()
+  ws.send("iden " .. os.getComputerID())
+
+  local data = ws.receive()
+
+  if not data then return false
+
+  if data == "ping" then ws.send("pong") end
+  if string.find("save", data) then
+    save_file(data)
+  end
+  if string.find("runf", data) then
+    run_file(data)
+  end
+  if 
+end
+
+connect()
 
 if err then
   print(err)
 else
   if ws then
-    print(ws.receive())
-    ws.send("Hello from ComputerCraft")
+    while true do
+      local result = handle()
+      if not handle() then
+        os.sleep(3)
+        connect()
+      end
+
+      if result == "shutdown" then
+        print("shutting down")
+        break
+      end
+    end
   end
 end
